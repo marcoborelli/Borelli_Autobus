cfb998dae6736472891316f6cda5a2634b7a296b
Marco Borelli <marco05.borelli@gmail.com>
2023-01-09 08:29:33 +0100

    Aggiungere i file di progetto.

---
+++ Borelli_Autobus/Autobus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Borelli_Autobus {
+    public class Autobus {
+        private string _numTelaio, _targa, _azienda;
+        private int _passMax;
+
+    }
+}
