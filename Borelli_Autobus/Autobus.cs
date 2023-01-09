61f63dff54918a762cf124de6ba5bccec81b2ddb
Marco Borelli <marco05.borelli@gmail.com>
2023-01-09 09:24:00 +0100

    Aggiunte tutte le funzioni specifiche richieste

--- Borelli_Autobus/Autobus.cs
+++ Borelli_Autobus/Autobus.cs
@@ -8,16 +8,67 @@ using System.Text.RegularExpressions;
 namespace Borelli_Autobus {
     public class Autobus {
         private string _numTelaio, _targa, _azienda;
-        private int _passMax;
+        private int _passMax, _pass;
+        private bool _fermo, _inDeposito;
         //costruttore
         public Autobus(string nTel, string targ, string aziend, int passMax) {
             this.NumeroTelaio = nTel;
             this.Targa = targ;
             this.Azienda = aziend;
             this.PasseggeriMax = passMax;
+            this.Fermo = true;
+            this.InDeposito = false;
         }
         public Autobus() {
 
+        }
+        //funzioni specifiche
+        public void Partenza() {
+            if (this.Fermo) {
+                this.Fermo = false;
+                this.InDeposito = false; //assumo che se parto lo tolgo dal deposito
+            } else {
+                throw new Exception("Il bus è già in movimento");
+            }
+        }
+        public void SalitaPasseggeri(int pass) {
+            if (this.Fermo && !this.InDeposito) {
+                this.Passeggeri += pass;
+            } else {
+                throw new Exception("Non si possono far salire passeggere se il bus è in movimento o se è in deposito");
+            }
+        }
+        public void Fermata() {
+            if (!this.Fermo) {
+                this.Fermo = true;
+            } else {
+                throw new Exception("Il bus è già fermo");
+            }
+        }
+        public void DiscesaPasseggeri(int pass) {
+            if (this.Fermo) {
+                this.Passeggeri -= pass;
+            } else {
+                throw new Exception("Non si possono far scendere passeggere se il bus è in movimento");
+            }
+        }
+        public void SpostaPasseggeri(Autobus a) {
+            if (a != null) {
+                if (this.Fermo && a.Fermo) {
+                    a.Passeggeri += this.Passeggeri;
+                    DiscesaPasseggeri(this.Passeggeri);
+                } else {
+                    throw new Exception("Entrambi i bus devono essere fermi per eseguire lo scambio");
+                }
+
+            } else {
+                throw new Exception("Inserire un bus valido");
+            }
+        }
+        public void Deposita() {
+            Fermata();
+            DiscesaPasseggeri(this.Passeggeri);
+            this.InDeposito = true;
         }
         //funzioni standard
         public override string ToString() {
@@ -80,6 +131,34 @@ namespace Borelli_Autobus {
                 } else {
                     throw new Exception("Inserire un valore maggiore di 0");
                 }
+            }
+        }
+        public bool Fermo {
+            get {
+                return _fermo;
+            }
+            set {
+                _fermo = value;
+            }
+        }
+        public int Passeggeri {
+            get {
+                return _pass;
+            }
+            set {
+                if (_pass + value >= 0 && _pass + value <= this.PasseggeriMax) {
+                    _pass += value;
+                } else {
+                    throw new Exception("Capienza massima raggiunta o tentativo di scendere sotto 0");
+                }
+            }
+        }
+        public bool InDeposito {
+            get {
+                return _inDeposito;
+            }
+            set {
+                _inDeposito = value;
             }
         }
     }
